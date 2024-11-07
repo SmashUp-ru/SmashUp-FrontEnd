031e4f7e4c06e91db1a334024b607fcf499be2df
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2024-11-07 13:50:30 +0300

    init: add husky

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,15 @@ function App() {
                 <a href='https://react.dev' target='_blank'>
                     <img src='' className='logo react' alt='React logo' />
                 </a>
+
+
+
+
+
+
+
+
+
             </div>
             <h1>Vite + React</h1>
             <div className='card'>
