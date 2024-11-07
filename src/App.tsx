773467208e11ab0f42db081cf455efaf03855529
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2024-11-07 14:04:25 +0300

    init: add shadcn-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import viteLogo from '/vite.svg';
+import { Button } from '@/components/ui/button.tsx';
 
 function App() {
     const [count, setCount] = useState(0);
@@ -17,7 +18,7 @@ function App() {
             </div>
             <h1 className='text-red-500 text-4xl'>Vite + React</h1>
             <div className='card'>
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+                <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
                 <p>
                     Edit <code>src/App.tsx</code> and save to test HMR
                 </p>
