b076cce54b82d9c5e4c74244083c1d5c6085ddb2
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2024-11-18 19:58:14 +0300

    fix: style of tabs list

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -12,7 +12,7 @@ const TabsList = React.forwardRef<
     <TabsPrimitive.List
         ref={ref}
         className={cn(
-            'inline-flex gap-x-4 bg-surfaceVariant items-center justify-center p-1 text-muted-foreground',
+            'inline-flex gap-x-4 items-center justify-center p-1 text-muted-foreground',
             className
         )}
         {...props}
@@ -27,7 +27,7 @@ const TabsTrigger = React.forwardRef<
     <TabsPrimitive.Trigger
         ref={ref}
         className={cn(
-            'inline-flex font-bold text-[18px] text-onSurface items-center justify-center whitespace-nowrap rounded-2xl px-4 py-2 text-sm transition-all disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-primary data-[state=active]:text-surface',
+            'inline-flex bg-surfaceVariant font-bold text-[18px] text-onSurface items-center justify-center whitespace-nowrap rounded-2xl px-4 py-2 text-sm transition-all disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-primary data-[state=active]:text-surface',
             className
         )}
         {...props}
