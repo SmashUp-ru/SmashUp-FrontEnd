326fa24c6e127bf2a8cd21eae523f093c09663c7
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2024-12-23 02:24:21 +0300

    feat: remove debug button from header

--- src/router/features/header/Header.tsx
+++ src/router/features/header/Header.tsx
@@ -2,12 +2,8 @@ import SearchBar from '@/router/features/header/SearchBar.tsx';
 import ProfileMenu from '@/router/features/header/ProfileMenu.tsx';
 import NotificationsMenu from '@/router/features/header/NotificationsMenu.tsx';
 import BackButton from '@/router/features/header/BackButton.tsx';
-import { Button } from '@/components/ui/button.tsx';
-import { useGlobalStore } from '@/store/global.ts';
 
 export default function Header() {
-    const { isLoading, updateIsLoading } = useGlobalStore();
-
     return (
         <div className='py-4 pr-4 flex items-center gap-x-12'>
             <div className='w-full flex items-center gap-x-4'>
@@ -15,13 +11,6 @@ export default function Header() {
                 <SearchBar />
             </div>
             <div className='flex items-center gap-x-6'>
-                <Button
-                    onClick={() => {
-                        updateIsLoading(!isLoading);
-                    }}
-                >
-                    press me
-                </Button>
                 <NotificationsMenu />
                 <ProfileMenu />
             </div>
