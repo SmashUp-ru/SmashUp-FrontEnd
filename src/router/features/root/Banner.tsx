55f65e8040eb3efe15cc09b6bc924ab06a91784b
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2024-12-23 15:08:07 +0300

    refactor: remove unused files

--- src/router/features/root/Banner.tsx
+++
@@ -1,7 +0,0 @@
-import banner from '@/assets/banner.png';
-
-export default function Banner() {
-    return (
-        <img src={banner} alt='Banner' className='border rounded-[50px] w-full' draggable={false} />
-    );
-}
