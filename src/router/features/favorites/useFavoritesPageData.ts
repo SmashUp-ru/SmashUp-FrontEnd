4ee98a7f0dc193e07f10fc4360986a6c2a779900
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2024-12-29 13:25:02 +0300

    fix: /favorites fullscreen skeleton

--- src/router/features/favorites/useFavoritesPageData.ts
+++ src/router/features/favorites/useFavoritesPageData.ts
@@ -5,53 +5,35 @@ import { axiosSession } from '@/lib/utils.ts';
 import { AxiosResponse } from 'axios';
 
 export function useFavoritesPageData() {
-    const startLoading = useGlobalStore((state) => state.startLoading);
-    const updateIsLoading = useGlobalStore((state) => state.updateIsLoading);
     const likes = useGlobalStore((state) => state.likes);
     const updateLikes = useGlobalStore((state) => state.updateLikes);
     const getMashupsByIds = useMashupStore((state) => state.getManyByIds);
 
-    const [isPlaylistPageLoading, setIsPlaylistPageLoading] = useState(false);
-    const [mashupsLoading, setMashupsLoading] = useState(false);
+    const [isPlaylistPageLoading, setIsPlaylistPageLoading] = useState(likes === null);
+    const [mashupsLoading, setMashupsLoading] = useState(likes === null);
 
     const [mashups, setMashups] = useState<Mashup[]>([]);
 
     useEffect(() => {
-        startLoading();
-        setIsPlaylistPageLoading(true);
+        axiosSession
+            .get(`${import.meta.env.VITE_BACKEND_URL}/mashup/get_all_likes`)
+            .then(
+                (
+                    r: AxiosResponse<{
+                        status: string;
+                        response: number[];
+                    }>
+                ) => {
+                    updateLikes(r.data.response);
+                }
+            )
+            .finally(() => setIsPlaylistPageLoading(false));
     }, []);
 
     useEffect(() => {
-        updateIsLoading(mashupsLoading || isPlaylistPageLoading);
-    }, [isPlaylistPageLoading, mashupsLoading]);
-
-    useEffect(() => {
-        if (likes === null) {
-            axiosSession
-                .get(`${import.meta.env.VITE_BACKEND_URL}/mashup/get_all_likes`)
-                .then(
-                    (
-                        r: AxiosResponse<{
-                            status: string;
-                            response: number[];
-                        }>
-                    ) => {
-                        updateLikes(r.data.response);
-                    }
-                )
-                .finally(() => setIsPlaylistPageLoading(false));
-        } else {
-            setIsPlaylistPageLoading(false);
-        }
-    }, []);
-
-    useEffect(() => {
-        if (likes) {
-            setMashupsLoading(true);
-            getMashupsByIds(likes)
-                .then((r) => setMashups(r))
-                .finally(() => setMashupsLoading(false));
-        }
+        getMashupsByIds(likes ? likes : [])
+            .then((r) => setMashups(r))
+            .finally(() => setMashupsLoading(false));
     }, [likes]);
 
     return {
