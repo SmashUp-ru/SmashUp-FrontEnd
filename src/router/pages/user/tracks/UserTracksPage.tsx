383977dec0c26d700d3b42c11b5f04ab24dc9a3e
LeonidM <wildhs.botm@gmail.com>
2024-12-31 18:44:58 +0300

    fix: mashup list at user page

--- src/router/pages/user/tracks/UserTracksPage.tsx
+++ src/router/pages/user/tracks/UserTracksPage.tsx
@@ -13,7 +13,6 @@ import { Skeleton } from '@/components/ui/skeleton.tsx';
 import { cn } from '@/lib/utils.ts';
 import { useToast } from '@/router/shared/hooks/use-toast.ts';
 import BaseToast from '@/router/shared/toasts/Base.tsx';
-import { useCurrentUserPlaylists } from '@/router/shared/hooks/useCurrentUserPlaylists.ts';
 
 export default function UserTracksPage() {
     const { toast } = useToast();
@@ -24,7 +23,6 @@ export default function UserTracksPage() {
     const getMashupsByIds = useMashupStore((state) => state.getManyByIds);
     const isPlaying = usePlayerStore((state) => state.isPlaying);
     const queueId = usePlayerStore((state) => state.queueId);
-    const { playlists } = useCurrentUserPlaylists();
 
     const [user, setUser] = useState<User | null>(null);
     const [mashups, setMashups] = useState<Mashup[]>([]);
@@ -127,7 +125,6 @@ export default function UserTracksPage() {
                 {mashups.map((mashup, idx) => (
                     <MashupSmallThumb
                         key={mashup.id}
-                        playlists={playlists}
                         mashup={mashup}
                         playlist={user.mashups}
                         indexInPlaylist={idx}
