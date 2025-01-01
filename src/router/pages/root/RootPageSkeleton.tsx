583aa6d507c53097567f3deb4993d4d675602b8d
LeonidM <wildhs.botm@gmail.com>
2025-01-01 17:39:04 +0300

    feat: add premiere list at root page

--- src/router/pages/root/RootPageSkeleton.tsx
+++ src/router/pages/root/RootPageSkeleton.tsx
@@ -24,6 +24,17 @@ export default function RootPageSkeleton() {
                         ))}
                 </div>
             </section>
+
+            <section className='flex flex-col gap-y-2.5'>
+                <Skeleton className='w-[268px] h-[38px] rounded-lg' />
+                <div className='grid grid-cols-3 gap-x-[25px] gap-y-[15px]'>
+                    {Array.from({ length: 10 })
+                        .slice(0, 6)
+                        .map((_, idx) => (
+                            <MashupSmallThumbSkeleton key={idx} />
+                        ))}
+                </div>
+            </section>
         </div>
     );
 }
