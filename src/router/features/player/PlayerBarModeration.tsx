0f174ba6a2d89655af52e9ed78fa301582109fb8
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2025-01-01 18:29:27 +0300

    fix: mashupseekslider not updating in moderation player

--- src/router/features/player/PlayerBarModeration.tsx
+++ src/router/features/player/PlayerBarModeration.tsx
@@ -44,7 +44,7 @@ export default function PlayerBarModeration() {
                 clearInterval(moderationIntervalRef.current);
             }
         };
-    }, [moderationPlayer, updateSeek]);
+    }, [moderationIsPlaying, updateSeek]);
 
     if (!moderationSrc) return null;
 
